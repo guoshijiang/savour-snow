657743931909b59128339683ac5e9e72710cd8a4
guoshijiang <guoshijiang2012@163.com>
2022-08-04 22:33:39 +0800

    sol token transfer sign support

--- wallet/sol/index.ts
+++ wallet/sol/index.ts
@@ -1,5 +1,6 @@
 const bs58 = require("bs58")
 const { derivePath, getPublicKey } = require('ed25519-hd-key')
+import * as SPLToken from "@solana/spl-token";
 import { PublicKey, Keypair, Transaction, SystemProgram } from "@solana/web3.js";
 const BigNumber = require("bignumber.js");
 
@@ -19,19 +20,47 @@ export function createSolAddress(seedHex: string, addressIndex: string) {
 
 
 export async function signSolTransaction(params) {
-    const { amount, to, nonce, decimal, privateKey } = params;
+    const { from, amount, to, mintAddress, nonce, decimal, privateKey } = params;
     const fromAccount = Keypair.fromSecretKey(new Uint8Array(Buffer.from(privateKey, "hex")), { skipValidation: true });
     const calcAmount = new BigNumber(amount).times(new BigNumber(10).pow(decimal)).toString();
     if (calcAmount.indexOf(".") !== -1) throw new Error("decimal 无效");
     let tx = new Transaction();
+    const toPubkey = new PublicKey(to);
+    const fromPubkey = new PublicKey(from);
     tx.recentBlockhash = nonce;
-    tx.add(
-        SystemProgram.transfer({
-            fromPubkey: fromAccount.publicKey,
-            toPubkey: new PublicKey(to),
-            lamports: calcAmount,
-        })
-    );
+    if(mintAddress){
+        const mint = new PublicKey(mintAddress);
+        const fromTokenAccount = await SPLToken.Token.getAssociatedTokenAddress(
+            SPLToken.ASSOCIATED_TOKEN_PROGRAM_ID,
+            SPLToken.TOKEN_PROGRAM_ID,
+            mint,
+            fromPubkey
+        );
+        const toTokenAccount = await SPLToken.Token.getAssociatedTokenAddress(
+            SPLToken.ASSOCIATED_TOKEN_PROGRAM_ID,
+            SPLToken.TOKEN_PROGRAM_ID,
+            mint,
+            toPubkey
+        );
+        tx.add(
+            SPLToken.Token.createTransferInstruction(
+                SPLToken.TOKEN_PROGRAM_ID,
+                fromTokenAccount,
+                toTokenAccount,
+                fromPubkey,
+                [fromAccount],
+                calcAmount
+            )
+        );
+    } else {
+        tx.add(
+            SystemProgram.transfer({
+                fromPubkey: fromAccount.publicKey,
+                toPubkey: new PublicKey(to),
+                lamports: calcAmount,
+            })
+        );
+    }
     tx.sign(fromAccount);
     return tx.serialize().toString("base64");
 }
@@ -39,7 +68,7 @@ export async function signSolTransaction(params) {
 /**
  * address
  * network type
- * @param params 
+ * @param params
  */
 export function verifySolAddress(params: any) {
     const { address } = params;
